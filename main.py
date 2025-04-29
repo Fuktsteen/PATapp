0ab70672f7f498be38ccde182a98a6d855dff87f
Fuktsteen <vak02@pm.me>
2025-04-29 16:22:01 +0300

    finally set up and created python template

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def write_to_file(time=0, distance=0, calories=0):
+    print(time, distance, calories)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
