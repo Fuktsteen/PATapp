41b05f8452ecb7b1a5bf3f4c90cc18324f3a2560
Fuktsteen <vak02@pm.me>
2025-04-30 23:52:55 +0300

    two left

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from PyQt5.QtCore import QSize, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout, QStackedWidget, QLineEdit, \
     QHBoxLayout, QLabel
 
-
 def save_to_file(file, mode, details):
     #print(f"Destination: {file}\nDetails: {details}")
     with open(file, mode) as f:
@@ -33,9 +32,9 @@ def set_physique():
 
 def calculate_calories(weight, distance):
     # Calories burned ≈ body mass (kg) × distance (km) × 1 kcal·kg⁻¹·km⁻¹
-    calories = int(weight) * int(distance)
+    calories = int(weight) * (int(distance) / 1000)
     print(f"Calculating calories..."
-          f"\t{weight} kg * {distance} km * 1 kcal*kg^(-1)*km^(-1) = {calories}"
+          f"\t{weight} kg * {int(distance) / 1000} km * 1 kcal*kg^(-1)*km^(-1) = {calories}"
           f"\tDone!")
     return calories
 
@@ -69,6 +68,13 @@ def check_input(input, mode):
         except (ValueError, AttributeError):
             print(f"Invalid input.")
             return False
+    elif mode == "physique":
+        try:
+            if int(input.strip()) < 0:
+                raise ValueError
+        except ValueError:
+            print(f"Invalid input.")
+            return False
     else:
         characters = list(input)
         try:
@@ -76,7 +82,7 @@ def check_input(input, mode):
                 raise IndexError
             parts = input.split(mode)
             if mode == ".":
-                first = 32
+                first = 31
                 second = 12
                 third = 99
                 lowest = 1
@@ -114,7 +120,6 @@ def fetch_session_details():
     for raw_session in session_data:
         fine_sessions.append(raw_session.split("-"))
     fine_sessions.pop()
-    #print(fine_sessions)
     return fine_sessions
 
 def edit_session_details():
@@ -174,20 +179,79 @@ class Menu(QWidget):
         layout = QVBoxLayout(self)
         self.draw_graphs = QPushButton("Draw graphs")
         self.new_session = QPushButton("Add new session details")
-        self.edit_session = QPushButton("Edit session details")
-        self.replace_physique = QPushButton("Replace existing physique")
+        self.edit_session = QPushButton()
+        self.replace_physique = QPushButton()
         self.exit_button = QPushButton("Exit")
 
         self.draw_graphs.clicked.connect(lambda: self.switch_view.emit(1))
         self.new_session.clicked.connect(lambda: self.switch_view.emit(1))
         self.edit_session.clicked.connect(lambda: self.switch_view.emit(1))
-        self.replace_physique.clicked.connect(lambda: self.switch_view.emit(1))
+        self.replace_physique.clicked.connect(lambda: self.switch_view.emit(2))
         self.exit_button.clicked.connect(lambda: exit())
         layout.addWidget(self.draw_graphs)
         layout.addWidget(self.new_session)
         layout.addWidget(self.edit_session)
         layout.addWidget(self.replace_physique)
         layout.addWidget(self.exit_button)
+
+    def showEvent(self, event):
+        self.edit_session.setText(f"Edit session details ({len(fetch_session_details())} saved sessions)")
+        self.replace_physique.setText(f"Replace existing physique ({get_file_content("physique.txt")} kg)")
+        super().showEvent(event)
+
+class EditSession(QWidget):
+    switch_view = pyqtSignal(int)
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout(self)
+        
+class Physique(QWidget):
+    switch_view = pyqtSignal(int)
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout(self)
+
+        # need layers for info, input and buttons
+        infoLayer = QHBoxLayout()
+        self.infoLabel = QLabel()
+        infoLayer.addWidget(self.infoLabel)
+
+        inputLayer = QHBoxLayout()
+        self.physiqueLabel = QLabel("Enter your weight in kg without decimals:")
+        self.physiqueInput = QLineEdit()
+        self.physiqueInput.textChanged.connect(lambda: self.input_checker(self.physiqueInput.text()))
+        inputLayer.addWidget(self.physiqueLabel)
+        inputLayer.addWidget(self.physiqueInput)
+
+        buttonLayer = QHBoxLayout()
+        self.backButton = QPushButton("Back")
+        self.backButton.clicked.connect(lambda: self.switch_view.emit(0))
+        self.saveButton = QPushButton("Save")
+        self.saveButton.clicked.connect(lambda: self.save_handler(self.physiqueInput.text()))
+        self.saveButton.setDisabled(True)
+        buttonLayer.addWidget(self.backButton)
+        buttonLayer.addWidget(self.saveButton)
+
+        layout.addLayout(infoLayer)
+        layout.addLayout(inputLayer)
+        layout.addLayout(buttonLayer)
+
+    def showEvent(self, event):
+        self.infoLabel.setText(f"Your current saved physique is {get_file_content('physique.txt')} kg."
+                                f"\nThis is used to estimate burned calories based on distance exercised:"
+                                f"\nCalories burned ≈ body mass (kg) × distance (km) × 1 kcal·kg⁻¹·km⁻¹")
+        super().showEvent(event)
+
+    def save_handler(self, weight):
+        save_to_file("physique.txt", "w", weight)
+        self.physiqueInput.setText("")
+        self.switch_view.emit(0)
+
+    def input_checker(self, candidate):
+        if check_input(candidate, "physique"):
+            self.saveButton.setEnabled(True)
+        else:
+            self.saveButton.setEnabled(False)
 
 class AddSession(QWidget):
     switch_view = pyqtSignal(int)
@@ -196,41 +260,62 @@ class AddSession(QWidget):
         layout = QVBoxLayout(self)
 
         # need date, time and distance layers
+        self.inputChecker = [ [".", False], [":", False], ["d", False] ]
+
         dateLayer = QHBoxLayout()
-        self.dateLabel = QLabel("Date exercised (in format dd.mm.yy): ")
+        self.dateLabel = QLabel("Date exercised (dd.mm.yy):")
         self.dateInput = QLineEdit()
-        self.dateInput.textChanged.connect(lambda: self.input_checker(self.dateInput.text(), ":"))
+        self.dateInput.textChanged.connect(lambda: self.input_checker(self.dateInput.text(), "."))
         dateLayer.addWidget(self.dateLabel)
         dateLayer.addWidget(self.dateInput)
 
         timeLayer = QHBoxLayout()
-        self.timeLabel = QLabel("Time exercised (in hh:mm:ss): ")
+        self.timeLabel = QLabel("Time exercised (hh:mm:ss):")
         self.timeInput = QLineEdit()
-        self.timeInput.textChanged.connect(lambda: self.input_checker(self.timeInput.text(), "."))
+        self.timeInput.textChanged.connect(lambda: self.input_checker(self.timeInput.text(), ":"))
         timeLayer.addWidget(self.timeLabel)
         timeLayer.addWidget(self.timeInput)
 
         matkaLayer = QHBoxLayout()
-        self.matkaLabel = QLabel("Distance travelled in meters without decimals: ")
+        self.matkaLabel = QLabel("Distance travelled in meters without decimals:")
         self.matkaInput = QLineEdit()
         self.matkaInput.textChanged.connect(lambda: self.input_checker(self.matkaInput.text(), "d"))
         matkaLayer.addWidget(self.matkaLabel)
         matkaLayer.addWidget(self.matkaInput)
 
+        buttonLayer = QHBoxLayout()
+        self.backButton = QPushButton("Back")
+        self.backButton.clicked.connect(lambda: self.switch_view.emit(0))
         self.submitButton = QPushButton("Save")
         self.submitButton.clicked.connect(lambda: self.save_handler(self.dateInput.text(), self.timeInput.text(), self.matkaInput.text()))
+        self.submitButton.setDisabled(True)
+        buttonLayer.addWidget(self.backButton)
+        buttonLayer.addWidget(self.submitButton)
 
         layout.addLayout(dateLayer)
         layout.addLayout(timeLayer)
         layout.addLayout(matkaLayer)
-        layout.addWidget(self.submitButton)
+        layout.addLayout(buttonLayer)
 
     def save_handler(self, date, time, distance):
-        print(f"debug {date} {time} {distance}")
+        save_to_file("sessions.txt", "a", f"{date}-{time}-{distance}-{calculate_calories(get_file_content("physique.txt"), distance)}\n")
+        self.dateInput.setText("")
+        self.timeInput.setText("")
+        self.matkaInput.setText("")
         self.switch_view.emit(0)
 
     def input_checker(self, candidate, mode):
-        print(f"debug {candidate} {mode}")
+        if check_input(candidate, mode):
+            for i in self.inputChecker:
+                if i[0] == mode:
+                    i[1] = True
+            if self.inputChecker[0][1] and self.inputChecker[1][1] and self.inputChecker[2][1]:
+                self.submitButton.setEnabled(True)
+        else:
+            for i in self.inputChecker:
+                if i[0] == mode:
+                    i[1] = False
+            self.submitButton.setDisabled(True)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -244,20 +329,18 @@ class MainWindow(QMainWindow):
         self.menuview.switch_view.connect(self.stack.setCurrentIndex)
         self.newseshview = AddSession()
         self.newseshview.switch_view.connect(self.stack.setCurrentIndex)
+        self.physiqueview = Physique()
+        self.physiqueview.switch_view.connect(self.stack.setCurrentIndex)
+
         self.stack.addWidget(self.menuview)
         self.stack.addWidget(self.newseshview)
-
+        self.stack.addWidget(self.physiqueview)
         self.stack.setCurrentIndex(0)
-
         self.setCentralWidget(self.stack)
 
 if __name__ == '__main__':
     app = QApplication([])
-
-    # Create a Qt widget, which will be our window.
     window = MainWindow()
-    window.show()  # IMPORTANT!!!!! Windows are hidden by default.
-
-    # Start the event loop.
+    window.show()
     app.exec()
     #cli_menu()
