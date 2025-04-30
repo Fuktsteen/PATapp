35d07875af0914b8b3c3e49304c12c1fbb1c5ce7
Fuktsteen <villekos@pm.me>
2025-04-30 11:06:28 +0300

    onto the UI

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+import time
+
 
 # Code for the Personal Activity Tracker, PATapp.
 
 def save_to_file(file, mode, details):
-    print(f"Destination: {file}\nDetails: {details}")
+    #print(f"Destination: {file}\nDetails: {details}")
     with open(file, mode) as f:
         f.write(details)
     print(f"Saved!")
 
 def get_file_content(file):
-    print(f"Source: {file}")
+    #print(f"Source: {file}")
     with open(file) as f:
         content = f.read()
-        print(f"Content: {content}")
+        #print(f"Content: {content}")
         return content
 
 def set_physique():
@@ -34,7 +36,10 @@ def calculate_calories(weight, distance):
     return calories
 
 def check_input(input, mode):
-    if mode == "d":
+    if input.strip() == "":
+        print(f"Invalid input.")
+        return False
+    elif mode == "d":
         try:
             if int(input) < 0:
                 raise ValueError
@@ -42,26 +47,45 @@ def check_input(input, mode):
         except ValueError:
             print(f"Invalid input.")
             return False
-    characters = list(input)
-    try:
-        if characters[2] != mode or characters[5] != mode:
-            raise IndexError
-        parts = input.split(mode)
-        if mode == ".":
-            first = 32
-            second = 12
-            third = 99
-            lowest = 1
-        else:
-            first = 99
-            second = 59
-            third = 59
-            lowest = 0
-        if int(parts[0]) < lowest or int(parts[0]) > first or int(parts[1]) < lowest or int(parts[1]) > second or int(parts[2]) < lowest or int(parts[2]) > third:
-            raise ValueError
-    except (IndexError, ValueError):
-        print(f"Invalid input.")
-        return False
+    elif mode == "heti":
+        try:
+            if input.strip().lower() == "y" or input.strip().lower() == "n":
+                return True
+            else:
+                raise AttributeError
+        except AttributeError:
+            print(f"Invalid input.")
+            return False
+    elif mode == "menu":
+        try:
+            if int(input.strip()) in [1, 2, 3, 4]:
+                return True
+            else:
+                raise ValueError
+        except (ValueError, AttributeError):
+            print(f"Invalid input.")
+            return False
+    else:
+        characters = list(input)
+        try:
+            if characters[2] != mode or characters[5] != mode:
+                raise IndexError
+            parts = input.split(mode)
+            if mode == ".":
+                first = 32
+                second = 12
+                third = 99
+                lowest = 1
+            else:
+                first = 99
+                second = 59
+                third = 59
+                lowest = 0
+            if int(parts[0]) < lowest or int(parts[0]) > first or int(parts[1]) < lowest or int(parts[1]) > second or int(parts[2]) < lowest or int(parts[2]) > third:
+                raise ValueError
+        except (IndexError, ValueError):
+            print(f"Invalid input.")
+            return False
     return True
 
 def session_details_input():
@@ -80,25 +104,45 @@ def session_details_input():
     save_to_file("sessions.txt", "a", f"{date}-{time}-{distance}-{calculate_calories(get_file_content("physique.txt"), distance)}\n")
 
 def cli_menu():
-    while True:
-        heti = input("Add new session details? (y/n): ")
-        if check_input(heti, "heti"):
-            if heti.strip().lower() == "y":
-                session_details_input()
-            else:
-                break
-    while True:
-        print(f"\nWelcome to PATapp!"
-              f"\n"
-              f"\n[1] Draw graphs"
-              f"\n[2] Add new session details"
-              f"\n[3] Replace existing physique ({get_file_content("physique.txt")} kg)"
-              f"\n")
-
-if __name__ == '__main__':
     try:
         open("physique.txt", "x")
         set_physique()
     except FileExistsError:
         pass
-    session_details_input()
+    while True:
+        heti = input("Add new session details? (y/n): ")
+        if check_input(heti, "heti"):
+            if heti.strip().lower() == "y":
+                session_details_input()
+                break
+            else:
+                break
+    running = True
+    while running:
+        print(f"\nWelcome to PATapp!"
+              f"\n"
+              f"\n[1] Draw graphs"
+              f"\n[2] Add new session details"
+              f"\n[3] Replace existing physique ({get_file_content("physique.txt")} kg)"
+              f"\n[4] Exit"
+              f"\n")
+        while True:
+            menuInput = input("What would you like to do? (give only a number): ")
+            if check_input(menuInput, "menu"):
+                if int(menuInput.strip()) == 1:
+                    print(f"WIP")
+                    break
+                elif int(menuInput.strip()) == 2:
+                    session_details_input()
+                    break
+                elif int(menuInput.strip()) == 3:
+                    set_physique()
+                    break
+                elif int(menuInput.strip()) == 4:
+                    running = False
+                    break
+    print(f"Thank you for using PATapp. Exiting...")
+    time.sleep(1)
+
+if __name__ == '__main__':
+    cli_menu()
